0ae15b8dab677d2e33abe38e72e21f224b45e1fa
zheen <suseyizheen@gmail.com>
2025-04-25 15:05:27 -0700

    Initial Commit

---
+++ BlackJack-SwiftUI/BlackJack_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJack_SwiftUIApp.swift
+//  BlackJack-SwiftUI
+//
+//  Created by Zheen Suseyi on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BlackJack_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
